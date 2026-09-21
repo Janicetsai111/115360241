c20d1463a16d0701df1f6e2c74ce754612d90754
Janicetsai111 <janice970111@gmail.com>
2026-09-21 20:55:52 +0800

    Add files via upload

---
+++ homework2.25/homework2.25/homework2.25/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	printf("        T\n");
+	printf("        T\n");
+	printf("TTTTTTTTT\n");
+	printf("        T\n");
+	printf("        T\n");
+	printf("MMMMMMMMM\n");
+	printf("       M \n");
+	printf("     M   \n");
+	printf("       M \n");
+	printf("MMMMMMMMM\n");
+	printf("  CCCCCC \n");
+	printf("C       C\n");
+	printf("C       C\n");
+	printf("C       C\n");
+	printf(" C     C \n");
+	return 0;
+}
---
+++ homework2.26/homework2.26/homework2.26/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include <stdlib.h>
+
+int main(void) {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf_s("%d %d", &num1, &num2);
+
+    if (num2 != 0 && num1 % num2 == 0)
+    {
+        printf("%d is a multiple of %d\n", num1, num2);
+    }
+    else
+    {
+        printf("%d is not a multiple of %d\n", num1, num2);
+    }
+
+    system("pause");
+    return 0;
+}
